16c871e52c3240bdd5ff9ff3260f6eb47ff39ed1
Rafael Ramalho <rafazelramalho19@gmail.com>
2020-02-27 17:55:36 +0000

    feat: improve emoji handler

--- api/emoji.js
+++ api/emoji.js
@@ -1,11 +1,29 @@
 "use strict";
-const $ = require("cheerio");
+const cheerio = require("cheerio");
+const axios = require("axios");
 
-exports.handler = function(event, context, callback) {
-  const { queryStringParameters } = event;
-  console.log(queryStringParameters);
-  callback(null, {
+export async function handler ({ queryStringParameters }, context) {
+  const { text, emoji, emojiEmpty } = queryStringParameters;
+
+  if(!text || !emoji || !emojiEmpty) {
+    return {
+      statusCode: 500,
+      body: 'You should specify the "text", "emoji" and "emojiEmpty" query parameters'
+    }
+  }
+
+  const { data } = await axios.get(`http://www.network-science.de/ascii/ascii.php?TEXT=${text}&x=50&y=8&FONT=banner&RICH=no&FORM=left&STRE=no&WIDT=80`);
+
+  const $ = cheerio.load(data);
+
+  const pre = $('TABLE PRE');
+  const ascii = pre.text();
+
+  const asciiWithoutHash = ascii.replace(/#/g, emoji);
+  const asciiWithoutSpaces = asciiWithoutHash.replace(/\ /g, emojiEmpty);
+
+  return {
     statusCode: 200,
-    body: queryStringParameters
-  });
+    body: asciiWithoutSpaces
+  };
 };
